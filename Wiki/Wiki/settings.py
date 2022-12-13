10342e169672eaa8f92d3e80e2e68a8ea9db730b
jaredbare <jab468@byu.edu>
2022-12-13 09:29:06 -0700

    add database

--- Wiki/Wiki/settings.py
+++ Wiki/Wiki/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from getpass import getpass
 from dotenv import load_dotenv
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,14 +80,10 @@ WSGI_APPLICATION = 'Wiki.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'wiki',
-        'USER': 'postgres',
-        'PASSWORD': 'bambooand',
-        'HOST': 'localhost'
-    }
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
 
 
