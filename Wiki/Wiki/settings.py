ba154528189a625228314343550d0e39eb94550b
jaredbare <jab468@byu.edu>
2022-12-13 11:35:33 -0700

    rearranged directory

--- Wiki/Wiki/settings.py
+++ Wiki/Wiki/settings.py
@@ -85,6 +85,7 @@ WSGI_APPLICATION = 'Wiki.wsgi.application'
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 DATABASES = {
+    
     "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
 
